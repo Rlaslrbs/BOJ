f1f840678be1971b08c586c31203fe12981c52ea
rlask.rbs <160216101+Rlaslrbs@users.noreply.github.com>
2025-06-09 14:10:26 +0000

    2025

--- 백준연습장/10816.py
+++ 백준연습장/10816.py
@@ -1,1 +1,43 @@
-n = int(input())
+import sys
+input = sys.stdin.readline()
+
+N = int(input())
+cards = [*map(int,input().split())]
+M = int(input())
+targets =[*map(int,input().split())]
+
+
+count = {}
+
+for card in cards:
+    if card in count:
+        count[card] +=1
+    else:
+        count[card] = 1
+
+
+
+
+
+
+
+
+
+
+
+
+lst=[]
+for i in card:
+    count = 0
+    low = 0
+    high = N - 1
+    while low <= high:
+        mid = (low + high // 2)
+        if target[mid] < i:
+            low = mid - 1
+        elif target[mid] > i:
+            high = mid + 1
+        else:
+            count +=1
+            lst.append(count)
+

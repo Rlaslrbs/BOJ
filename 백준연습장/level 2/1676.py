de5f768f0789377a429d3cae94bdf38ba28e6bed
rlask.rbs <160216101+Rlaslrbs@users.noreply.github.com>
2025-06-14 14:12:58 +0000

    1676 문제해결

--- 백준연습장/level 2/1676.py
+++ 백준연습장/level 2/1676.py
@@ -1,7 +1,33 @@
+"""
 def factorial(n):
     if n == 1:
         return 1
     return n * factorial(n-1)
 
-for i in 12345:
-    print(i)
+n = int(input())
+
+
+count = 0
+reverse_str = ""
+for i in str(factorial(n)):
+    reverse_str = i + reverse_str
+
+for i in reverse_str:
+
+    i = int(i)
+    if i == 0:
+        count += 1
+    else:
+        print(count)
+        break
+ """  
+n = int(input())
+i = 5
+count = 0 
+while n >= i:
+    if n // i != 0:
+        count +=(n//i)
+        i*=5
+
+print(count)
+    
